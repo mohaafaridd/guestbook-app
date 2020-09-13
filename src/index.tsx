015f976ef6873be4d992db447e1f8e1ed893ebef
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-09-13 19:28:43 +0200

    installed chakra ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <CSSReset />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
