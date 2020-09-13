9faec0075dc60f5fda3eae3f6ab11162872fc492
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-09-13 22:52:16 +0200

    created createdMessage mutation

---
+++ src/graphql/message/CreateMessageMutation.ts
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client/core';
+
+export const CREATE_MESSAGE = gql`
+  mutation CreateMessage($data: CreateMessageInput!) {
+    createMessage(data: $data) {
+      _id
+      content
+      createdAt
+      updatedAt
+      author {
+        _id
+        name
+        email
+      }
+
+      parent {
+        _id
+        content
+        author {
+          _id
+          name
+        }
+      }
+    }
+  }
+`;
