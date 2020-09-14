2701142bfe1cc0884e54b34b986b8f1f87cc66ed
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-09-14 16:45:10 +0200

    created mutation to update message

---
+++ src/graphql/message/UpdateMessageMutation.ts
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client/core';
+
+export const UPDATE_MESSAGE = gql`
+  mutation UpdateMessage($data: UpdateMessageInput!) {
+    updateMessage(data: $data) {
+      _id
+      content
+      createdAt
+      updatedAt
+      author {
+        _id
+        name
+        email
+      }
+
+      parent {
+        _id
+        content
+        author {
+          _id
+          name
+        }
+      }
+    }
+  }
+`;
