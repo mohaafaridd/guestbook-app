1018e35896254c00de583c4a8bc71b2f274c4291
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-09-14 11:48:28 +0200

    created a Message Card component

---
+++ src/components/Messages/Card/index.tsx
@@ -0,0 +1,37 @@
+import { Box, Text } from '@chakra-ui/core';
+import React from 'react';
+import { Message } from '../../../interfaces/Message';
+import LinkButton from '../../common/LinkButton';
+import moment from 'moment';
+
+interface MessageCardArgs {
+  message: Message;
+}
+
+export const MessageCard = ({ message }: MessageCardArgs) => {
+  return (
+    <Box
+      border='1px'
+      borderRadius='md'
+      borderColor='gray.200'
+      p={2}
+      rounded='md'
+      boxShadow='sm'
+    >
+      <LinkButton
+        to={`/users/${message.author._id}`}
+        color='gray.700'
+        fontWeight='semibold'
+        variant='link'
+        fontSize={14}
+      >
+        {message.author.name}
+      </LinkButton>
+      <Text fontSize={12} color='gray.500'>
+        {moment(message.createdAt).format('DD-MM-YYYY hh:mm A')}
+      </Text>
+
+      <Text>{message.content}</Text>
+    </Box>
+  );
+};
--- src/components/Messages/List/index.tsx
+++ src/components/Messages/List/index.tsx
@@ -1,5 +1,7 @@
+import { Stack } from '@chakra-ui/core';
 import React, { useContext } from 'react';
 import { MessageContext } from '../../../context/Message/messageContext';
+import { MessageCard } from '../Card';
 
 interface MessageListArgs {
   loading: boolean;
@@ -8,5 +10,11 @@ interface MessageListArgs {
 export const MessageList = ({ loading = false }: MessageListArgs) => {
   const { messages } = useContext(MessageContext);
   if (loading) return <h1>Loading</h1>;
-  return <div>{messages.length}</div>;
+  return (
+    <Stack>
+      {messages.map((message, i) => (
+        <MessageCard message={message} key={i} />
+      ))}
+    </Stack>
+  );
 };
