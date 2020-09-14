3ce3e88d6c6503dd056055d170851b31003b4d92
Mohammed Farid <dev.mohammedfarid@gmail.com>
2020-09-14 17:15:55 +0200

    enabled delete reply by mutation

--- src/components/Reply/Card/index.tsx
+++ src/components/Reply/Card/index.tsx
@@ -1,6 +1,10 @@
-import { Box, Text } from '@chakra-ui/core';
+import { useMutation } from '@apollo/client';
+import { Box, Flex, Grid, IconButton, Text, useToast } from '@chakra-ui/core';
 import moment from 'moment';
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../context/Auth/authContext';
+import { MessageContext } from '../../../context/Message/messageContext';
+import { DELETE_MESSAGE } from '../../../graphql/message/DeleteMessageMutation';
 import { Message } from '../../../interfaces/Message';
 import LinkButton from '../../common/LinkButton';
 
@@ -9,6 +13,37 @@ interface ReplyCardArgs {
 }
 
 export const ReplyCard = ({ reply }: ReplyCardArgs) => {
+  const toast = useToast();
+  const { authenticated, user } = useContext(AuthContext);
+  const { setMessage, message: contextReply } = useContext(MessageContext);
+
+  const [deleteReply, { loading: deleteLoading }] = useMutation(DELETE_MESSAGE);
+
+  const onDelete = async () => {
+    try {
+      const variables = {
+        id: reply._id,
+      };
+      const { data } = await deleteReply({ variables });
+      const deletedReply: Message = data.deleteMessage;
+      console.log('deletedReply', deletedReply);
+      // removeMessage(deletedReply);
+      toast({
+        description: 'Message Deleted',
+        status: 'success',
+        duration: 1500,
+        position: 'bottom-left',
+      });
+    } catch (error) {
+      toast({
+        description: 'Could not delete the message',
+        status: 'error',
+        duration: 1500,
+        position: 'bottom-left',
+      });
+    }
+  };
+
   return (
     <Box
       border='1px'
@@ -17,18 +52,54 @@ export const ReplyCard = ({ reply }: ReplyCardArgs) => {
       p={2}
       rounded='md'
     >
-      <LinkButton
-        to={`/users/${reply.author._id}`}
-        color='gray.700'
-        fontWeight='semibold'
-        variant='link'
-        fontSize={14}
+      <Grid
+        templateRows={['repeat(4, auto)', '1fr']}
+        templateColumns={['1fr', 'repeat(2, auto)']}
       >
-        {reply.author.name}
-      </LinkButton>
-      <Text fontSize={12} color='gray.500'>
-        {moment(reply.createdAt).format('DD-MM-YYYY hh:mm A')}
-      </Text>
+        <Box>
+          <LinkButton
+            to={`/users/${reply.author._id}`}
+            color='gray.700'
+            fontWeight='semibold'
+            variant='link'
+            fontSize={14}
+          >
+            {reply.author.name}
+          </LinkButton>
+          <Text fontSize={12} color='gray.500'>
+            {moment(reply.createdAt).format('DD-MM-YYYY hh:mm A')}
+          </Text>
+        </Box>
+
+        {authenticated && user?._id === reply.author._id && (
+          <Flex
+            flexDir={['column', 'row']}
+            justify='flex-end'
+            alignItems={['stretch', 'center']}
+          >
+            <IconButton
+              size='sm'
+              aria-label='edit message'
+              variantColor='green'
+              icon='edit'
+              // onClick={onEdit}
+              isDisabled={contextReply?._id === reply._id}
+            />
+            <IconButton
+              onClick={onDelete}
+              mt={[2, 0]}
+              ml={[0, 2]}
+              size='sm'
+              aria-label='delete-message'
+              variant='outline'
+              variantColor='red'
+              icon='delete'
+              isDisabled={contextReply?._id === reply._id}
+              isLoading={deleteLoading}
+            />
+          </Flex>
+        )}
+      </Grid>
 
       {reply.content}
     </Box>
